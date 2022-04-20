3f4df4fcc91ad312bc6f2b8061a81593b4e680ad
Xtreshebot <botxtreshe@gmail.com>
2022-04-20 12:41:39 +0700

    Hachimitsu

---
+++ plugins/donasi.js
@@ -0,0 +1,32 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `
+┌─「 Donasi • Pulsa 」
+│ • *Sematfren:* [${global.ppulsa}]
+│ • *Smartfren:* [${global.ppulsa2}]
+❏────
+
+┌─「 Donasi • Non Pulsa 」
+│ • *Ovo:* [${global.povo}]
+│ • *Gopay:* [${global.pgopay}]
+│ • *Dana:* [${global.pdana}]
+❏────
+`
+const templateButtons = [
+    {index: 1, urlButton: {displayText: '📍 Github', url: sgh}},
+    {index: 2, urlButton: {displayText: '🌎 Official Group', url: sgc}},
+    {index: 3, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
+    {index: 4, quickReplyButton: {displayText: 'Owner', id: '.owner'}},
+]
+let tm = {
+text: text,
+footer: global.wm,
+templateButtons: templateButtons,
+image: {url: fla + 'Donasi'}
+}
+conn.sendMessage(m.chat, tm, m)
+}
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
---
+++ plugins/info-script.js
@@ -0,0 +1,29 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `*${htki} SOURCE CODE ${htka}*
+┌─❏
+│✦ *MULTI DEVICE*
+│• *Script:* 
+│➔ 🔒 Private
+│• *Base:*
+│➔ https://github.com/kannachann/kannaBOT-MD
+❏────
+`
+const templateButtons = [
+    {index: 1, urlButton: {displayText: '👤 My Github', url: sgh}},
+    {index: 2, urlButton: {displayText: '🌎 Official Group', url: sgc}},
+    {index: 3, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
+    {index: 4, quickReplyButton: {displayText: 'Donasi', id: '.donasi'}},
+]
+let tm = {
+text: text,
+footer: global.wm,
+templateButtons: templateButtons,
+image: thumb
+}
+conn.sendMessage(m.chat, tm, m)
+}
+handler.help = ['sc']
+handler.tags = ['info']
+handler.command = /^(s(ourcode|c))$/i
+
+export default handler
